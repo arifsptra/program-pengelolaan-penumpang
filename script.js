222842dda739254466e55659e4c9b1ef4f832d9e
arifsptra <111202113480@mhs.dinus.ac.id>
2021-11-28 12:32:40 +0700

    Menambahkan fitur hapus penumpang

--- script.js
+++ script.js
@@ -32,3 +32,31 @@ var tambahPenumpang = function (namaPenumpang, penumpang) {
     }
   }
 };
+var hapusPenumpang = function (namaPenumpang, penumpang) {
+  // jika tidak ada penumpang
+  if (penumpang == 0) {
+    // tampilkan pesan bahwa angkot kosong
+    console.log("Angkot Kosong \nTidak mungkin ada penumpang yang turun");
+    // kembalikan isi array & keluar dari function
+    return penumpang;
+  } else {
+    // telusuri seluruh kursi dari awal
+    for (i = 0; i <= penumpang.length; i++) {
+      // jika nama penumpang sesuai
+      if (penumpang[i] == namaPenumpang) {
+        // hapus penumpang dengan mengubah namanya menjadi undefined
+        penumpang[i] = undefined;
+        // kembalikan isi array & keluar dari function
+        return penumpang;
+      }
+      // jika tidak ada nama yang sesuai
+      // penumpang[i] != namaPenumpang
+      else if (i == penumpang.length - 1) {
+        // tampilkan pesan kesalahan
+        console.log("Maaf tidak ada penumpang yang bernama " + namaPenumpang);
+        // kembalikan isi array & keluar dari function
+        return penumpang;
+      }
+    }
+  }
+};
